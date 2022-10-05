65b078db835fd6601b7b4aa1b3771d452e430afd
Etisha Garg <gargetisha@gmail.com>
2022-10-05 17:38:28 +0530

    Adding config for goerli test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_PRIVATE_KEY = process.env.PRIVATE_GOERLI_ACCOUNT_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_URL;
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`]
+    },
+  },
 };
